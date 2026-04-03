9bf3f507704cbbcebf2bf74142292187bed82807
optimisticdeveloperxoxo <bhagwane.sarthakprof@gmail.com>
2026-04-03 20:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -427,7 +427,6 @@ def auto_categorise(text):
     return None
 
 if __name__ == "__main__":
-    init_db()
-    print("✅  Database ready.")
-    print("🚀  SpendSense → http://127.0.0.1:5000")
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
